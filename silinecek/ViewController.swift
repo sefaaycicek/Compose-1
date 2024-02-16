82281a2a8c27ee67ccc0144ac6410eeabb64fd45
Sefa Aycicek <saycicek@ipucubilisim.com.tr>
2024-02-16 08:31:08 +0300

    Initial Commit

---
+++ silinecek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  silinecek
+//
+//  Created by Sefa Aycicek on 16.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
